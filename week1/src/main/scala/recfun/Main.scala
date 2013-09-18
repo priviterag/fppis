c2f238dd2b5055acd1e5ea1e0ac82c548c498e1d
Giuseppe Privitera <priviterag@gmail.com>
2013-09-18 22:53:35 +0100

    avoid return

--- week1/src/main/scala/recfun/Main.scala
+++ week1/src/main/scala/recfun/Main.scala
@@ -50,11 +50,14 @@ object Main {
     * Exercise 3
     */
   def countChange(money: Int, coins: List[Int]): Int = {
-    if (coins.isEmpty) return 0
-    if (money < 0) return 0
-    if (money == 0) return 1
+    if (coins.isEmpty || money < 0)
+      0
     else {
-      countChange(money, coins.tail) + countChange(money - coins.head, coins)  
+      if (money == 0)
+        1
+      else {
+        countChange(money, coins.tail) + countChange(money - coins.head, coins)
+      }
     }
   }
 }
