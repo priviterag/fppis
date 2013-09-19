def69355e3b2d4eaafa06c17c7114807bbf5632b
Giuseppe Privitera <priviterag@gmail.com>
2013-09-19 04:41:00 +0100

    better exists

--- week2/src/main/scala/funsets/FunSets.scala
+++ week2/src/main/scala/funsets/FunSets.scala
@@ -66,14 +66,7 @@ object FunSets {
    * Returns whether there exists a bounded integer within `s`
    * that satisfies `p`.
    */
-  def exists(s: Set, p: Int => Boolean): Boolean = {
-    def iter(a: Int): Boolean = {
-      if (a > 1000) false
-      else if (contains(s, a)) p(a) || iter(a + 1)
-      else iter(a + 1)
-    }
-    iter(-1000)  
-  }
+  def exists(s: Set, p: Int => Boolean): Boolean = !forall(s, (x: Int) => !p(x))
 
   /**
    * Returns a set transformed by applying `f` to each element of `s`.
