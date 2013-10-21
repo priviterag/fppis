4c69c69dc3d8b33d963bbc5168db5b25827e3cc1
Giuseppe Privitera <priviterag@gmail.com>
2013-10-21 14:21:36 +0100

    subtract function

--- week6/src/main/scala/forcomp/Anagrams.scala
+++ week6/src/main/scala/forcomp/Anagrams.scala
@@ -115,7 +115,7 @@ object Anagrams {
     *  Note: the resulting value is an occurrence - meaning it is sorted
     *  and has no zero-entries.
     */
-  def subtract(x: Occurrences, y: Occurrences): Occurrences = ???
+  def subtract(x: Occurrences, y: Occurrences): Occurrences = x diff y
 
   /** Returns a list of all anagram sentences of the given sentence.
     *
