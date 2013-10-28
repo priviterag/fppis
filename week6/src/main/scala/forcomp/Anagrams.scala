ac8fd67b03e0809e520ce834cf236a0ccd7f4069
Giuseppe Privitera <priviterag@gmail.com>
2013-10-28 23:08:14 +0000

    refactored dictionaryByOccurrences

--- week6/src/main/scala/forcomp/Anagrams.scala
+++ week6/src/main/scala/forcomp/Anagrams.scala
@@ -59,7 +59,8 @@ object Anagrams {
     *    List(('a', 1), ('e', 1), ('t', 1)) -> Seq("ate", "eat", "tea")
     *
     */
-  lazy val dictionaryByOccurrences: Map[Occurrences, List[Word]] = dictionary.groupBy(e => wordOccurrences(e))
+  lazy val dictionaryByOccurrences: Map[Occurrences, List[Word]] =
+    dictionary.groupBy(e => wordOccurrences(e)).withDefaultValue(Nil)
 
   /** Returns all the anagrams of a given word. */
   def wordAnagrams(word: Word): List[Word] = dictionaryByOccurrences(wordOccurrences(word))
