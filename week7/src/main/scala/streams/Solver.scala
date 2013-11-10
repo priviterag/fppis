3d3e01023a147b1ed43ee85a4009a6933b230fee
Giuseppe Privitera <priviterag@gmail.com>
2013-11-10 19:48:04 +0000

    newneighborsonly function

--- week7/src/main/scala/streams/Solver.scala
+++ week7/src/main/scala/streams/Solver.scala
@@ -38,7 +38,9 @@ trait Solver extends GameDef {
    * make sure that we don't explore circular paths.
    */
   def newNeighborsOnly(neighbors: Stream[(Block, List[Move])],
-                       explored: Set[Block]): Stream[(Block, List[Move])] = ???
+                       explored: Set[Block]): Stream[(Block, List[Move])] = {
+    (neighbors filter { case (block, move) => !explored.contains(block)}).toStream
+  }
 
   /**
    * The function `from` returns the stream of all possible paths
--- week7/src/test/scala/streams/BloxorzSuite.scala
+++ week7/src/test/scala/streams/BloxorzSuite.scala
@@ -81,6 +81,15 @@ class BloxorzSuite extends FunSuite {
     }
   }
 
+  test("newNeighborsOnly level 1") {
+    new Level1 {
+      val nno = Set((Block(Pos(2,1),Pos(3,1)), List(Down,Left,Up))).toStream
+      assert(newNeighborsOnly(Set((Block(Pos(1,2),Pos(1,3)), List(Right,Left,Up)),
+              (Block(Pos(2,1),Pos(3,1)), List(Down,Left,Up))).toStream,
+              Set(Block(Pos(1,2),Pos(1,3)), Block(Pos(1,1),Pos(1,1)))) == nno)
+    }
+  }
+
   test("optimal solution for level 1") {
     new Level1 {
       assert(solve(solution) == Block(goal, goal))
